92a4982a80b7120650fc98714944a3243acbb479
Shoaib-ali7860 <54176556+Shoaib-ali7860@users.noreply.github.com>
2021-02-07 17:01:01 +0530

    Add files via upload

---
+++ pages/hireme.js
@@ -0,0 +1,5 @@
+import Link from 'next/Link'
+import Layout from './components/layout.js'
+
+const HireMe=()=>(<Layout title="Hire Me"></Layout>) 
+export default HireMe
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Link from 'next/Link'
+import Layout from './components/layout.js'
+const Index=()=>{
+  return <Layout title="Home">
+  <p>this is home page thanks for visits</p></Layout>
+}
+export default Index
