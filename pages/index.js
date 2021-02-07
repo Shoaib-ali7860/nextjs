24748d41659ab98fd6095cfbf7ce4c4c32e3fd14
Shoaib-ali7860 <54176556+Shoaib-ali7860@users.noreply.github.com>
2021-02-07 16:52:36 +0530

    Delete index.js

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-import Link from 'next/Link'
-import Layout from './components/layout.js'
-const Index=()=>{
-  return <Layout title="Home">
-  <p>this is home page thanks for visits</p></Layout>
-}
-export default Index
